61dae49c5f3ddce9aa7f3106fd05c01687af623e
ohader <ohader@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-10-28 16:42:27 +0000

    [TASK] Update ext_emconf.php
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/a21glossary/trunk@53534 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "a21glossary".
 #
-# Auto generated 17-10-2011 13:21
+# Auto generated 28-10-2011 18:42
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -33,7 +33,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
 	'version' => '0.8.3',
-	'_md5_values_when_last_written' => 'a:14:{s:9:"ChangeLog";s:4:"353a";s:10:"README.txt";s:4:"7cad";s:24:"class.tx_a21glossary.php";s:4:"e05a";s:21:"ext_conf_template.txt";s:4:"76ef";s:12:"ext_icon.gif";s:4:"dff2";s:17:"ext_localconf.php";s:4:"b780";s:14:"ext_tables.php";s:4:"91a6";s:14:"ext_tables.sql";s:4:"f3ae";s:28:"icon_tx_a21glossary_main.gif";s:4:"a4ed";s:16:"locallang_db.xml";s:4:"bfaf";s:7:"tca.php";s:4:"3058";s:14:"doc/manual.sxw";s:4:"f0a4";s:20:"static/constants.txt";s:4:"72bf";s:16:"static/setup.txt";s:4:"26d7";}',
+	'_md5_values_when_last_written' => 'a:15:{s:9:"ChangeLog";s:4:"353a";s:10:"README.txt";s:4:"7cad";s:24:"class.tx_a21glossary.php";s:4:"ad79";s:21:"ext_conf_template.txt";s:4:"76ef";s:12:"ext_icon.gif";s:4:"dff2";s:17:"ext_localconf.php";s:4:"b780";s:14:"ext_tables.php";s:4:"91a6";s:14:"ext_tables.sql";s:4:"f3ae";s:28:"icon_tx_a21glossary_main.gif";s:4:"a4ed";s:13:"locallang.xml";s:4:"efca";s:16:"locallang_db.xml";s:4:"bfaf";s:7:"tca.php";s:4:"d419";s:14:"doc/manual.sxw";s:4:"f0a4";s:20:"static/constants.txt";s:4:"1ff2";s:16:"static/setup.txt";s:4:"9e19";}',
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.1.0-0.0.0',
